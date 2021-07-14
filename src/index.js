f1dd30c3e872381d54b5e3a3db094f65cd1ce6f3
LuisEliasTec <luiselias_1@hotmail.com>
2021-07-14 13:07:17 -0600

    index.js fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello world!')
+    response.send('Hello World!')
 })
 
 app.listen(3000, function () {
